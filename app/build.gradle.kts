69200c9dfc9a2df2dd48731c64fc7180e976d77d
Nikita <japskiddin@gmail.com>
2023-10-23 12:11:18 +0800

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,108 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+android {
+    namespace = "io.github.japskiddin.imagetowallpaper"
+    buildToolsVersion = AppConfig.buildTools
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        applicationId = "io.github.japskiddin.imagetowallpaper"
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+        versionCode = AppConfig.versionCode
+        versionName = AppConfig.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = AppConfig.compose
+    }
+    packaging {
+        jniLibs {
+            excludes += listOf(
+                "**/kotlin/**",
+                "META-INF/androidx.*",
+                "META-INF/proguard/androidx-*"
+            )
+        }
+        resources {
+            excludes += listOf(
+                "/META-INF/*.kotlin_module",
+                "**/kotlin/**",
+                "**/*.txt",
+                "**/*.xml",
+                "**/*.properties",
+                "META-INF/DEPENDENCIES",
+                "META-INF/LICENSE",
+                "META-INF/LICENSE.txt",
+                "META-INF/license.txt",
+                "META-INF/NOTICE",
+                "META-INF/NOTICE.txt",
+                "META-INF/notice.txt",
+                "META-INF/ASL2.0",
+                "META-INF/*.version",
+                "META-INF/androidx.*",
+                "META-INF/proguard/androidx-*"
+            )
+        }
+    }
+    applicationVariants.all {
+        val variant = this
+        variant.outputs.map { it as com.android.build.gradle.internal.api.BaseVariantOutputImpl }
+            .forEach { output ->
+                val outputFileName = "image_to_wallpaper-${variant.flavorName}-${variant.versionName}-${buildType.name}.apk"
+                output.outputFileName = outputFileName
+            }
+    }
+}
+
+tasks.withType<JavaCompile> {
+    val compilerArgs = options.compilerArgs
+    compilerArgs.addAll(
+        listOf(
+            "-Xlint:unchecked",
+            "-Xlint:deprecation"
+        )
+    )
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.graphics)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    androidTestImplementation(libs.compose.ui.test.junit4)
+    debugImplementation(libs.compose.ui.tooling)
+    debugImplementation(libs.compose.ui.test.manifest)
+}
---
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -0,0 +1,10 @@
+//app level config constants
+object AppConfig {
+    const val compileSdk = 34
+    const val minSdk = 21
+    const val targetSdk = 34
+    const val buildTools = "34.0.0"
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+    const val compose = "1.5.3"
+}
