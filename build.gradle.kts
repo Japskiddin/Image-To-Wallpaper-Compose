ca0b38ef5ba2edd01fc0d2089de501bc0a6831bd
Nikita <japskiddin@gmail.com>
2023-11-13 12:19:38 +0800

    * Added version catalog update plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,12 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ben.manes.versions)
+    alias(libs.plugins.version.catalog.update)
 }
 
 tasks.register("clean", Delete::class) {
     delete(layout.buildDirectory)
 }
+
+apply("${project.rootDir}/buildscripts/toml-updater-config.gradle")
