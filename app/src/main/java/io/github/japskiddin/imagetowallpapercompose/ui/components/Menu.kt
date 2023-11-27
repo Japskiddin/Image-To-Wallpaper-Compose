264e707a7930aa30e97569406908485d44928279
Nikita <japskiddin@gmail.com>
2023-11-27 21:02:20 +0800

    * Refactoring

--- app/src/main/java/io/github/japskiddin/imagetowallpapercompose/ui/components/Menu.kt
+++ app/src/main/java/io/github/japskiddin/imagetowallpapercompose/ui/components/Menu.kt
@@ -1,11 +1,15 @@
 package io.github.japskiddin.imagetowallpapercompose.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,8 +21,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.japskiddin.imagetowallpapercompose.R
 import io.github.japskiddin.imagetowallpapercompose.WallpaperType
+import io.github.japskiddin.imagetowallpapercompose.utils.PreviewWithTheme
 
 @Composable
 fun Menu(
@@ -120,5 +127,35 @@ fun PopupMenu(
             },
             onClick = onSetWallpaperBoth
         )
+    }
+}
+
+@Composable
+fun MenuButton(icon: Int, desc: Int, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    IconButton(
+        onClick = { onClick() },
+        modifier = modifier
+            .padding(8.dp)
+            .background(
+                color = MaterialTheme.colorScheme.primary,
+                shape = CircleShape
+            )
+    ) {
+        Icon(
+            painter = painterResource(id = icon),
+            contentDescription = stringResource(id = desc),
+            tint = MaterialTheme.colorScheme.onPrimary,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun MenuButtonPreview() {
+    PreviewWithTheme {
+        MenuButton(
+            icon = R.drawable.ic_gallery,
+            desc = R.string.select_image,
+            onClick = {})
     }
 }
